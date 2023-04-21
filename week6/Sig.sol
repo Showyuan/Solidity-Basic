62b392dd98d455ec3c8338f41996445642627c0c
showyuan <showyuan.liang@gmail.com>
2023-04-21 16:23:03 +0800

    week6 update

--- week6/Sig.sol
+++ week6/Sig.sol
@@ -27,7 +27,7 @@ contract VerifySig {
     }
 
     function getEthSignedMessageHash(bytes32 _messageHash) public pure returns (bytes32) {
-        return keccak256(abi.encodePacked(_messageHash));
+        return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32",_messageHash));
     }
 
     function recover(bytes32 _ethSignedMessageHash, bytes memory _sig)
